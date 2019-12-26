c3710ce935340721463d7feca2847b28721d68fd
rohan <52232895+rbr7@users.noreply.github.com>
2019-12-26 08:39:44 +0530

    Update local-run.py

--- wgcna_scaled/local-run.py
+++ wgcna_scaled/local-run.py
@@ -7,14 +7,14 @@ from scp import SCPClient
 
 ############### configurations starts ###################
 # instance config
-pem_path = '/home/bn7/Documents/pem_files/WGCNA_Instance_KP.pem'
-instance_dns = ['ec2-18-222-233-106.us-east-2.compute.amazonaws.com', 'ec2-3-16-22-102.us-east-2.compute.amazonaws.com', 'ec2-18-218-1-94.us-east-2.compute.amazonaws.com', 'ec2-3-16-43-173.us-east-2.compute.amazonaws.com', 'ec2-18-220-141-109.us-east-2.compute.amazonaws.com']
-username = 'ubuntu'
+pem_path = ##
+instance_dns = ##
+username = ##
 
 # credentials config
-# bitbucket
-bb_uname = 'kcbg475'
-bb_pwd = ''
+# ##
+bb_uname = ##
+bb_pwd = ##
 
 # aws
 my_access_key = ''
@@ -32,34 +32,7 @@ def fileprint(message):
         fp.close()
 
 # this is the job to be run on each instance
-def run_job(instance_dns, username, pem_path, id_path):
-    fileprint("running job "+id_path)
-    fileprint("instance: "+instance_dns)
-    fileprint(id_path)
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(
-                    instance_dns,
-                    username=username,
-                    key_filename=pem_path)
-    # send file with ids to the instance
-    with SCPClient(client.get_transport()) as scp:
-        scp.put(id_path, '~/'+id_path)
-    # clone the repo in the instance using git
-    stdin, stdout, stderr = client.exec_command('git clone https://'+bb_uname+':'+bb_pwd+'@bitbucket.org/elucidatainc/masswgcna.git')
-    exit_status = stdout.channel.recv_exit_status()          # Blocking call
-    # upgrade apt and install pip & aws 
-    # install r and it's packages too
-    stdin, stdout, stderr = client.exec_command('cd masswgcna; ./configure.sh > output-configure.out')
-    exit_status = stdout.channel.recv_exit_status()          # Blocking call
-    # configure aws key
-    stdin, stdout, stderr = client.exec_command('aws configure set aws_access_key_id '+ my_access_key)
-    stdin, stdout, stderr = client.exec_command('aws configure set aws_secret_access_key '+ my_secret_key)
-    # run r script here
-    stdin, stdout, stderr = client.exec_command('cd masswgcna; ./masswgcna.sh -t '+ dataset_type +' -g ~/'+id_path+' -f '+output_folder+' > output-masswgcna.out')
-    exit_status = stdout.channel.recv_exit_status()          # Blocking call
-    # close connection
-    client.close()
+###################################
 
 # function to divide major file into x smaller files where x is the number of instances
 def divide_id_file(id_file, div_len=len(instance_dns)):
